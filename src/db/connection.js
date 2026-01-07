76d01c5b59c37e0adb14bee21a6407e2088db7e3
LeonardoLBraga <Leonardobraga24@gmail.com>
2026-01-07 20:04:27 -0300

    fix: trocando ssl para conexao com o postgree

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,9 @@
-// src/db/connection.js
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production"
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
